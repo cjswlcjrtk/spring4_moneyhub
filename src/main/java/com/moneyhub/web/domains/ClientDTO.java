3fcdd985a57b3e95804b7d61f902c3fe0cfb98bc
username <cjswlcjrtk@naver.com>
2019-10-18 12:56:50 +0900

    "20191018 12:56"

--- src/main/java/com/moneyhub/web/domains/ClientDTO.java
+++ src/main/java/com/moneyhub/web/domains/ClientDTO.java
@@ -3,7 +3,9 @@ package com.moneyhub.web.domains;
 import java.io.Serializable;
 import org.springframework.stereotype.Component;
 
-@Component
+import lombok.Data;
+
+@Component @Data
 public class ClientDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private String cid,
